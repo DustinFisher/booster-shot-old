7dfcf1c7e69ca8aecc3642a4ec524be77d26c499
Dustin Fisher <dustin@dustinfisher.com>
2016-02-14 20:56:04 -0500

    need to remove the erb application template file for the slim one

--- app/template.rb
+++ app/template.rb
@@ -6,6 +6,7 @@ copy_file   "app/assets/javascripts/application.js", :force => true
 copy_file   "app/assets/javascripts/bootstrap.js.coffee", :force => true
 copy_file   "app/controllers/static_pages_controller.rb"
 copy_file   "app/helpers/application_helper.rb", :force => true
+remove_file "app/views/layouts/application.html.erb"
 copy_file   "app/views/layouts/application.slim", :force => true
 copy_file   "app/views/shared/_flash.slim"
 copy_file   "app/views/static_pages/index.slim"
