3d6e17783d3faf1114c52e92ee674eb6c2ee16d1
Dustin Fisher <dustin@dustinfisher.com>
2016-07-04 10:18:36 -0400

    routes file changed in rails 5 need to change sub to insert

--- config/template.rb
+++ config/template.rb
@@ -2,7 +2,8 @@ template "config/database.example.yml.tt"
 remove_file "config/database.yml"
 remove_file "config/secrets.yml"
 
-gsub_file "config/routes.rb", /  # root 'welcome#index'/ do
+insert_into_file "config/routes.rb",
+                 :after => /Rails\.application.*\n/ do
   '  root "static_pages#index"'
 end
 
